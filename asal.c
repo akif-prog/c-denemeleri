5f3dfbb0ce9b9992f92fda4a545103b1473f2df5
Akif Kapsız <69682503+akif-prog@users.noreply.github.com>
2026-02-09 16:57:40 +0300

    Create asal.c

---
+++ asal.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+	
+	int ilkSayi, sonSayi, i, j;
+	
+	printf("ilk sayi girin:");
+	scanf("%d",&ilkSayi);
+	
+	printf("son sayi girin:");
+	scanf("%d",&sonSayi);
+	
+	for(i=ilkSayi; i<=sonSayi; i++){
+		
+		int asal=1;
+		
+		if(i<2){
+			asal=0;
+		}
+		
+		for(j=2; i/2>=j; j++){
+			
+			
+			if(i%j==0){
+				asal=0;
+				break;
+			}
+			
+		}
+		
+		if(asal==1){
+				printf("%d ",i);
+			}
+	}
+	
+	return 0;
+}
